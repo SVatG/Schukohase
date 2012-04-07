2b13af7cccd60c96063e36660c29b3acc105e466
Lorenz Diener <lorenzd@gmail.com>
2012-04-07 12:59:11 +0200

    Things fixed.

--- project/palflip.c
+++ project/palflip.c
@@ -9,12 +9,17 @@
 #include "Loader.h"
 #include "RainbowTable.h"
 
-uint8_t colpos = 0;
-uint8_t copypos = 0;
-void hblank() {
-	dmaCopyHalfWords( 0, &rainbowTable[colpos], PALRAM_A+copypos, 5 );
+volatile uint8_t colpos = 0;
+volatile uint8_t copypos = 0;
+void hblank() {	
+// 	dmaCopyHalfWords( 0, &rainbowTable[(colpos%256)], PALRAM_A+1, 2 );
+	dmaCopyHalfWords( 0, &rainbowTable[((64+colpos)%256)], PALRAM_A+2, 2 );
+	dmaCopyHalfWords( 0, &rainbowTable[((128+colpos)%256)], PALRAM_A+3, 2 );
+	dmaCopyHalfWords( 0, &rainbowTable[((192+colpos)%256)], PALRAM_A+4, 2 );
+	dmaCopyHalfWords( 0, &rainbowTable[((colpos)%256)], PALRAM_A+5, 2 );
+	
 	colpos++;
-	copypos += 3;
+// 	copypos+=9;
 }
 
 uint8_t flip = 0;
@@ -22,37 +27,24 @@ uint8_t flip = 0;
 // Cap/flip
 void capflip() {	 
 	 if( flip ) {
-		vramSetBankA( VRAM_A_TEXTURE_SLOT2 );
-		vramSetBankD( VRAM_D_LCD );
+		vramSetBankB( VRAMCNT_B_BG_VRAM_A_OFFS_128K );
+		vramSetBankC( VRAM_C_LCD );
 		REG_DISPCAPCNT =
-			DCAP_BANK( 3 ) |
+			DCAP_BANK( 2 ) |
 			DCAP_SIZE( 3 ) |
-			DCAP_SRC( 1 ) |
+			DCAP_SRC( 0 ) |
 			DCAP_MODE( 0 ) |
 			DCAP_ENABLE;
-// 		rtexture =
-// 			DS_TEX_ADDRESS( VRAM_D ) |
-// 			DS_TEX_SIZE_S_256 |
-// 			DS_TEX_SIZE_T_256 |
-// 			DS_TEX_FORMAT_RGB |
-// 			DS_TEX_GEN_TEXCOORD;
 	}
 	else {
-		vramSetBankA( VRAM_A_LCD );
-		vramSetBankD( VRAM_D_TEXTURE_SLOT3 );
+		vramSetBankB( VRAM_B_LCD );
+		vramSetBankC( VRAMCNT_C_BG_VRAM_A_OFFS_128K );
 		REG_DISPCAPCNT =
-			DCAP_BANK( 0 ) |
+			DCAP_BANK( 1 ) |
 			DCAP_SIZE( 3 ) |
-			DCAP_SRC( 1 ) |
+			DCAP_SRC( 0 ) |
 			DCAP_MODE( 0 ) |
 			DCAP_ENABLE;
-
-// 		rtexture =
-// 			DS_TEX_ADDRESS( VRAM_C ) | // It's actually VRAM A at slot 2.
-// 			DS_TEX_SIZE_S_256 |
-// 			DS_TEX_SIZE_T_256 |
-// 			DS_TEX_FORMAT_RGB |
-// 			DS_TEX_GEN_TEXCOORD;
 	}
 	flip^=1;
 }
@@ -62,70 +54,51 @@ void palflip_init() {
 	irqSet( IRQ_HBLANK, hblank );
 	irqEnable( IRQ_HBLANK );
 	
-	DISPCNT_A=DISPCNT_MODE_5|DISPCNT_3D|DISPCNT_BG0_ON|DISPCNT_BG3_ON|DISPCNT_ON;
-
-	VRAMCNT_A = VRAMCNT_A_LCDC;
-
-	int dx = icos(128)/26.5;
-	int dy = isin(128)/26.5;
-	BG2PA_B = dx;
-	BG2PB_B = dy;
-	BG2PC_B = -dy;
-	BG2PD_B = dx;
-	BG2X_B = 4000;
-	BG2Y_B = 10000;
-
-	// Set up voxelcubes
-	VRAMCNT_D=VRAMCNT_D_LCDC;
-	VRAMCNT_F=VRAMCNT_F_LCDC;
-
-	loadVRAMIndirect( "nitro:/textures.pal4", VRAM_LCDC_D,16384);
-
-	for(int i=0;i<16;i++) VRAM_LCDC_F[i]=MakeRGB15(i+16,i+16,i+16);
-
-	VRAMCNT_D=VRAMCNT_D_TEXTURE_OFFS_0K;
-	VRAMCNT_F=VRAMCNT_F_TEXTURE_PALETTE_SLOT_0;
-
-	DSInit3D();
-	DSViewport(0,0,255,191);
-
-	DSSetControl(DS_TEXTURING|DS_ANTIALIAS);
-	DSClearParams(26,26,26,0,63);
-
-	DSSetPaletteOffset(0,DS_TEX_FORMAT_PAL4);
-
-	DSMatrixMode(DS_PROJECTION);
-	DSLoadIdentity();
-	DSPerspectivef(100,256.0/192.0,1,1024);
+	DISPCNT_A=DISPCNT_MODE_5|DISPCNT_3D|DISPCNT_BG2_ON|DISPCNT_BG3_ON|DISPCNT_ON;
 
 	// Background
-	VRAMCNT_B = VRAMCNT_B_BG_VRAM_A_OFFS_0K;
-	VRAMCNT_C = VRAMCNT_C_BG_VRAM_A_OFFS_128K;
+	VRAMCNT_A = VRAMCNT_B_BG_VRAM_A_OFFS_0K;
+	VRAMCNT_B = VRAMCNT_B_BG_VRAM_A_OFFS_128K;
+	VRAMCNT_C = VRAMCNT_C_LCDC;
 
-	BG3CNT_A = BGxCNT_EXTENDED_BITMAP_8 | BGxCNT_BITMAP_SIZE_256x256 | BGxCNT_OVERFLOW_TRANSPARENT | BGxCNT_BITMAP_BASE_0K;
+
+	BG2PA_A = 252;
+	BG2PB_A = 0;
+	BG2PC_A = 0;
+	BG2PD_A = 252;
+	BG2X_A = 0;
+	BG2Y_A = 0;
+	
+	BG3CNT_A = BGxCNT_EXTENDED_BITMAP_8 | BGxCNT_BITMAP_SIZE_256x256 | BGxCNT_OVERFLOW_WRAP | BGxCNT_BITMAP_BASE_0K;
 	BG3CNT_A = (BG3CNT_A&~BGxCNT_PRIORITY_MASK)|BGxCNT_PRIORITY_0;
-	BG3PA_A = (1 << 8);
-	BG3PB_A = 0;
-	BG3PC_A = 0;
-	BG3PD_A = (1 << 8);
-	BG3X_A = 0;
-	BG3Y_A = 0;
-
+	
+	BG2CNT_A = BGxCNT_EXTENDED_BITMAP_16 | BGxCNT_BITMAP_SIZE_256x256 | BGxCNT_OVERFLOW_WRAP | BGxCNT_BITMAP_BASE_128K;
+	BG2CNT_A = (BG2CNT_A&~BGxCNT_PRIORITY_MASK)|BGxCNT_PRIORITY_1;
 	
 	load8bVRAMIndirect( "nitro:/gfx/allstars.img.bin", VRAM_A_OFFS_0K,256*192*2);
-// 	loadVRAMIndirect( "nitro:/gfx/alls.pal.bin", PALRAM_A,256*2);
-}
-
-void objectShow(int t) {
-	static uint8_t ri = 0;
-	DSMatrixMode(DS_POSITION);
-	DSLoadIdentity();
-
-	DSSwapBuffers(0);
 }
 
 uint8_t palflip_update( uint32_t t ) {
 	capflip();
+
+	uint16_t* master_bright = (uint16_t*)(0x400006C);
+	if(t % 16 > 14) {
+		memset( master_bright, (1<<7) | (16), 2 );
+	}
+	else {
+		memset( master_bright, (1<<7) | (0), 2 );
+	}
+	
+	int dx = icos(isin(t*10)/5.0)/5.5;
+	int dy = isin(isin(t*10)/5.0)/5.5;
+
+	BG3PA_A=dx;
+	BG3PB_A=dy;
+	BG3PC_A=-dy;
+	BG3PD_A=dx;
+	BG3X_A=-128*dx-92*dy+(128<<8);
+	BG3Y_A=+128*dy-92*dx+(92<<8);
+	BG3X_A-=t*50;
 	
 	return 0;
 }
